3953dc2941414f71a17b77d95fb406737d3bb8ab
eb0247 <164549024+eb0247@users.noreply.github.com>
2026-07-27 18:57:14 +0900

    API명세서에 맞춰 추가

--- src/main/java/com/eum/hello_lux_quiz/dto/LifeDbRequestDto.java
+++ src/main/java/com/eum/hello_lux_quiz/dto/LifeDbRequestDto.java
@@ -1,6 +1,7 @@
 package com.eum.hello_lux_quiz.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,4 +21,12 @@ public class LifeDbRequestDto {
 
     @JsonProperty("like")
     private String like;
+
+    private String event;
+
+    @JsonProperty("photo_url")
+    private String photoUrl;
+
+    private String category;
 }
+
