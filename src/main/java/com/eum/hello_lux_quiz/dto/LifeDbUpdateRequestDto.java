38212f230441ff297de6d5463aa52ed812ca3d0c
eb0247 <164549024+eb0247@users.noreply.github.com>
2026-08-04 23:39:27 +0900

    Update LifeDbUpdateRequestDto.java

--- src/main/java/com/eum/hello_lux_quiz/dto/LifeDbUpdateRequestDto.java
+++ src/main/java/com/eum/hello_lux_quiz/dto/LifeDbUpdateRequestDto.java
@@ -1,5 +1,7 @@
 package com.eum.hello_lux_quiz.dto;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,11 +13,23 @@ public class LifeDbUpdateRequestDto {
     @JsonProperty("memory_id")
     private Integer memoryId;
 
-    private String family;
+    //> List<FamilyDto> 변경
+    private List<FamilyDto> family;
+    
     private String hometown;
     private String job;
     private String place;
 
     @JsonProperty("like")
     private String like;
+
+    // 수동 생성자
+    public LifeDbUpdateRequestDto(Integer memoryId, List<FamilyDto> family, String hometown, String job, String place, String like) {
+        this.memoryId = memoryId;
+        this.family = family;
+        this.hometown = hometown;
+        this.job = job;
+        this.place = place;
+        this.like = like;
+    }
 }
