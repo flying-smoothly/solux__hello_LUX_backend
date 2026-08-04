0bea87416a72a1d7e1f7181e35a6f3a480e26a05
eb0247 <164549024+eb0247@users.noreply.github.com>
2026-08-04 23:37:23 +0900

    Update LifeDbRequestDto.java

--- src/main/java/com/eum/hello_lux_quiz/dto/LifeDbRequestDto.java
+++ src/main/java/com/eum/hello_lux_quiz/dto/LifeDbRequestDto.java
@@ -1,5 +1,7 @@
 package com.eum.hello_lux_quiz.dto;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
@@ -14,7 +16,9 @@ public class LifeDbRequestDto {
     @JsonProperty("record_date")
     private String recordDate;
 
-    private String family;
+    // 💡 기존 String family -> List<FamilyDto> family로 변경
+    private List<FamilyDto> family;
+    
     private String hometown;
     private String job;
     private String place;
@@ -28,5 +32,20 @@ public class LifeDbRequestDto {
     private String photoUrl;
 
     private String category;
-}
 
+    // 수동 생성자
+    public LifeDbRequestDto(String title, String recordDate, List<FamilyDto> family, 
+                            String hometown, String job, String place, String like, 
+                            String event, String photoUrl, String category) {
+        this.title = title;
+        this.recordDate = recordDate;
+        this.family = family;
+        this.hometown = hometown;
+        this.job = job;
+        this.place = place;
+        this.like = like;
+        this.event = event;
+        this.photoUrl = photoUrl;
+        this.category = category;
+    }
+}
