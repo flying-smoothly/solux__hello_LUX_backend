a9fbc8dc2533e7e27e3202cf9938570cf7f0df27
eb0247 <164549024+eb0247@users.noreply.github.com>
2026-08-07 19:31:57 +0900

    Update QuizItemRepository.java

--- src/main/java/com/eum/hello_lux_quiz/repository/QuizItemRepository.java
+++ src/main/java/com/eum/hello_lux_quiz/repository/QuizItemRepository.java
@@ -1,6 +1,7 @@
 package com.eum.hello_lux_quiz.repository;
 
 import com.eum.hello_lux_quiz.domain.QuizItem;
+import com.eum.hello_lux_quiz.domain.QuizItemId; //  복합키 클래스 import
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,10 +9,10 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface QuizItemRepository extends JpaRepository<QuizItem, Integer> { // 💡 JpaRepository<..., Long> -> Integer로 변경
+public interface QuizItemRepository extends JpaRepository<QuizItem, QuizItemId> { // Integer -> QuizItemId로 변경
 
     // 특정 퀴즈 세트(setId)에 속한 문항 리스트 조회
-    List<QuizItem> findBySetId(Integer setId); // 💡 Long -> Integer로 변경
+    List<QuizItem> findBySetId(Integer setId);
 
-    Optional<QuizItem> findBySetIdAndQuizNum(Integer setId, Integer quizNum); // 💡 Long -> Integer로 변경
+    Optional<QuizItem> findBySetIdAndQuizNum(Integer setId, Integer quizNum);
 }
