97aaed8563d86ee332c65c6abf37063f897722cb
eb0247 <164549024+eb0247@users.noreply.github.com>
2026-08-07 23:25:37 +0900

    Add files via upload

--- src/main/java/com/eum/hello_lux_quiz/repository/QuizFeedbackRepository.java
+++ src/main/java/com/eum/hello_lux_quiz/repository/QuizFeedbackRepository.java
@@ -3,7 +3,10 @@ package com.eum.hello_lux_quiz.repository;
 import com.eum.hello_lux_quiz.domain.QuizFeedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface QuizFeedbackRepository extends JpaRepository<QuizFeedback, Integer> { // 💡 Long -> Integer 변경
+
+    List<QuizFeedback> findBySetId(Integer setId);
 }
